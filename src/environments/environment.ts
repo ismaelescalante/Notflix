4684f71de42556a923da13a6ecab19e4a9fcf30c
Ismael Escalante <96469912+ismaelescalante@users.noreply.github.com>
2023-10-18 10:05:01 +0200

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environments = {
-    API_KEY:
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGMyODMzZDAzNjgxZjIzNDFhYjRiNDRiZjgyMzgyNSIsInN1YiI6IjY1MTNlNGFlMDQ5OWYyMDBhYmJjZWYxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YZyDizfmpYNl8uhd-JHq0L2e7Z7B9VwRLb1hIv_wjjo',
-   };
