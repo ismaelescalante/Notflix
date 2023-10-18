69d6afbf485f38be7307b86e57a8acd97fd5c4ac
Ismael Escalante <ismael.escalante@bosonit.com>
2023-10-18 10:01:31 +0200

    update environments

--- src/app/core/interceptors/request.interceptor.ts
+++ src/app/core/interceptors/request.interceptor.ts
@@ -1,5 +1,6 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environments } from 'src/environments/environment';
 
